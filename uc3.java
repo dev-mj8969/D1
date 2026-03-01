1d9bd584b2a660684f617fcde8ce138c49bc70a5
dev-mj8969 <mj8969@srmist.edu.in>
2026-03-01 21:49:42 +0530

    String Join

---
+++ uc3.java
@@ -0,0 +1,12 @@
+public class uc2 {
+
+     public static void main(String[] args) {
+        
+        System.out.println(String.join("  ", " *** ", " *** ", "**** ", " ****"));
+        System.out.println(String.join("  ", "*   *", "*   *", "*   *", "*    "));
+        System.out.println(String.join("  ", "*   *", "*   *", "**** ", " *** "));
+        System.out.println(String.join("  ", "*   *", "*   *", "*    ", "    *"));
+        System.out.println(String.join("  ", " *** ", " *** ", "*    ", "**** "));
+    }
+}
+ 
